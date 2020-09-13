7f1fe53a1b589f44891edc901cda65fa1356e25a
Vishav Bangotra <vishavbangotra@Vishavs-MacBook-Air.local>
2020-09-13 18:39:49 +0530

    Issue resolved

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import {Route, Switch} from "react-router-dom";
+import NavbarC from './Components/Navbar.js'
 
 function App() {
   return (
     <Switch>
       <Route>
-
+        <NavbarC />
       </Route > 
     </Switch>
 
---
+++ src/Components/UpperNav.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './css/UpperNav.css'
+import userlogo from './../static/icons/user.png'
+class UpperNav extends Component{
+  render(){
+    return(
+      <div className='topbar'>
+        <a className="login-signup" href="#">LOGIN</a>
+        <a className="login-signup signup" href="#">SIGNUP</a>
+        <img src={userlogo} id='user-logo' alt="broken"></img>
+      </div>
+    )
+  }
+}
+export default UpperNav;
