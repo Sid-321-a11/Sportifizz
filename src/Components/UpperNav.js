326725ecf79be5b13f238bc5ecf7b2f9818778be
Vishav Bangotra <vishavbangotra@Vishavs-MacBook-Air.local>
2020-09-13 18:46:13 +0530

    added upper nav

--- src/Components/UpperNav.js
+++ src/Components/UpperNav.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import './css/UpperNav.css'
-import userlogo from './../static/icons/user.png'
+// import userlogo from './../static/icons/user.png'
 class UpperNav extends Component{
   render(){
     return(
       <div className='topbar'>
         <a className="login-signup" href="#">LOGIN</a>
         <a className="login-signup signup" href="#">SIGNUP</a>
-        <img src={userlogo} id='user-logo' alt="broken"></img>
+        {/* <img src={userlogo} id='user-logo' alt="broken"></img> */}
       </div>
     )
   }
