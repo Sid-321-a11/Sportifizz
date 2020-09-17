2d3a2d2a9bbb1b2d7937e7d3c38e9d64458f8876
Siddharth Ratuti <siddharthraturi2000@gmail.com>
2020-09-17 21:00:11 +0530

    final

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,12 +1,9 @@
 import React from 'react'
 import {Carousel} from "react-bootstrap";
 import './Home.css'
-<<<<<<< HEAD
-import Product from './Product';
-=======
 import './../index.css'
-import FetchProducts from './FetchProducts.js'
->>>>>>> 834ad820b7bc77f5a7dde65606733f97390f8822
+import Product from "./Product";
+
 function Home() {
     return (
       <div className="car">
@@ -14,11 +11,7 @@ function Home() {
         <Carousel.Item>
           <img
             className="d-block w-100"
-<<<<<<< HEAD
-            src="https://preview.redd.it/jq55u00zkqyy.jpg?width=960&crop=smart&auto=webp&s=7dcf542bcc3c6cb32ddf0839ff27d8ff693353b6"
-=======
             src="https://images.unsplash.com/photo-1540747913346-19e32dc3e97e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1467&q=80"
->>>>>>> 834ad820b7bc77f5a7dde65606733f97390f8822
             alt="First slide"
           />
           <Carousel.Caption>
@@ -50,9 +43,8 @@ function Home() {
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
-<<<<<<< HEAD
-
-    <p className="b">Bats</p>
+    
+      <p className="b">Bats</p>
     <div className="home-row">
 
     </div>
@@ -64,7 +56,7 @@ function Home() {
     rating={5}
     image="https://nb.scene7.com/is/image/NB/0dcplebo_nb_03_i?$pdpflexf2$&wid=440&hei=440&qlt=35&fmt=webp"
     />
-    
+
     <Product
     id="1"
     title="The lean startup:how innovation starts"
@@ -79,13 +71,13 @@ function Home() {
     rating={5}
     image="https://nb.scene7.com/is/image/NB/0dcplebo_nb_03_i?$pdpflexf2$&wid=440&hei=440&qlt=35&fmt=webp"
     />
-    
-    
-    
-  
+
+
+
+
     </div>
  <div className="home-row">
- 
+
  <Product
     id="1"
     title="The lean startup:how innovation starts"
@@ -107,17 +99,11 @@ function Home() {
     rating={5}
     image="https://nb.scene7.com/is/image/NB/0dcplebo_nb_03_i?$pdpflexf2$&wid=440&hei=440&qlt=35&fmt=webp"
     />
-    
- 
+
+
  </div>
 
-
-
-        </div>
-=======
-      <FetchProducts />
     </div>
->>>>>>> 834ad820b7bc77f5a7dde65606733f97390f8822
     )
 }
 export default Home
