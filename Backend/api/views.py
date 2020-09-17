9741119fde961bf7a81fb2d3dcff11d6208d41f9
Vishav Bangotra <vishavbangotra@Vishavs-MacBook-Air.local>
2020-09-17 16:31:52 +0530

    Test git 3

--- Backend/api/views.py
+++ Backend/api/views.py
@@ -45,3 +45,44 @@ def UpdateProductView(request, pk):
         serializer.save()
         return Response(serializer.data)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+##########
+
+@csrf_exempt
+@api_view(['GET'])
+def ListProducts(request):
+    if request.method == 'GET':
+        Products = Product.objects.all()
+        ProductsResponse = ProductSerializer(Products, many=True)
+        return Response(ProductsResponse.data)
+
+@api_view(['GET'])
+def DetailProduct(request,pk):
+    product = get_object_or_404(Product,pk=pk)
+    ProductsResponse = ProductSerializer(product, many=False)
+    return Response(ProductsResponse.data)
+
+@csrf_exempt
+@api_view(['POST'])
+def CreateProductView(request):
+    if request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['DELETE'])
+def DeleteProductView(request,pk):
+    product = get_object_or_404(Product,pk=pk)
+    product.delete()
+    return redirect('/')
+
+@api_view(['PUT', 'GET'])
+def UpdateProductView(request, pk):
+    product = get_object_or_404(Product,pk=pk)
+    serializer = ProductSerializer(data=request.data, instance=product)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
